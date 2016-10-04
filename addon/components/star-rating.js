ba7731775e7f667d380dc4b29bf727ee274365cd
Thomas McNiven <hello@vevix.net>
2016-10-05 07:31:54 +1000

    Cleanup & schedule afterRender

--- addon/components/star-rating.js
+++ addon/components/star-rating.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 const {
   Component,
   get,
-  set
+  set,
+  run
 } = Ember;
 
 const StarRatingComponent = Component.extend({
@@ -19,28 +20,31 @@ const StarRatingComponent = Component.extend({
 
   didInsertElement() {
     this._super(...arguments);
-    const options = get(this, 'options') || {};
-    const reference = this.$().rateYo(options);
-    reference.rateYo('rating', get(this, 'rating'));
+    run.scheduleOnce('afterRender', () => {
+      const options = get(this, 'options') || {};
+      const reference = this.$().rateYo(options);
+      reference.rateYo('rating', get(this, 'rating'));
 
-    reference.on('rateyo.set', (e, data) => {
-      const onSet = get(this, 'onSet');
-      if (onSet !== undefined) {
-        onSet(data.rating);
-      }
-    });
+      reference.on('rateyo.set', (e, data) => {
+        const onSet = get(this, 'onSet');
+        if (onSet !== undefined) {
+          onSet(data.rating);
+        }
+      });
 
-    reference.on('rateyo.change', (e, data) => {
-      const onChange = get(this, 'onChange');
-      if (onChange !== undefined) {
-        onChange(data.rating);
-      }
+      reference.on('rateyo.change', (e, data) => {
+        const onChange = get(this, 'onChange');
+        if (onChange !== undefined) {
+          onChange(data.rating);
+        }
+      });
     });
   },
 
   willDestroyElement() {
     this._super(...arguments);
     this.$().off('rateyo.set').off('rateyo.change');
+    this.$().rateYo('destroy');
   }
 });
 
